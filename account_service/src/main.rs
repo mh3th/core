db9898d2d16c93fc15e1bcaaee49511c41441c05
mh3th <wordlead@gmail.com>
2024-08-29 09:37:27 +0300

    add metrics base

--- account_service/src/main.rs
+++ account_service/src/main.rs
@@ -1,12 +1,19 @@
 use axum::{
-    body::Bytes, extract::MatchedPath, http::{HeaderMap, Request}, response::Html, routing::get, Router
+    body::Bytes,
+    extract::{MatchedPath, Request},
+    http::HeaderMap,
+    middleware::{self, Next},
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
 };
 use listenfd::ListenFd;
-use std::time::Duration;
-use tokio::{net::TcpListener, signal};
+use metrics_exporter_prometheus::{Matcher, PrometheusBuilder, PrometheusHandle};
+use std::{future::ready, time::Duration};
+use tokio::{net::TcpListener, signal, time::Instant};
 use tower::ServiceBuilder;
-use tower_http::{compression::CompressionLayer, timeout::TimeoutLayer};
 use tower_http::{classify::ServerErrorsFailureClass, trace::TraceLayer};
+use tower_http::{compression::CompressionLayer, timeout::TimeoutLayer};
 use tracing::{info_span, Span};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -25,9 +32,88 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let app = Router::new()
+    let (_main_server, _metrics_server) = tokio::join!(start_main_server(), start_metrics_server());
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
+
+async fn handler_404() -> Html<&'static str> {
+    Html("<h1>Not Found</h1>")
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+}
+
+fn setup_metrics_recorder() -> PrometheusHandle {
+    const EXPONENTIAL_SECONDS: &[f64] = &[
+        0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+    ];
+
+    PrometheusBuilder::new()
+        .set_buckets_for_metric(
+            Matcher::Full("http_requests_duration_seconds".to_string()),
+            EXPONENTIAL_SECONDS,
+        )
+        .unwrap()
+        .install_recorder()
+        .unwrap()
+}
+
+async fn track_metrics(req: Request, next: Next) -> impl IntoResponse {
+    let start = Instant::now();
+    let path = if let Some(matched_path) = req.extensions().get::<MatchedPath>() {
+        matched_path.as_str().to_owned()
+    } else {
+        req.uri().path().to_owned()
+    };
+    let method = req.method().clone();
+
+    let response = next.run(req).await;
+
+    let latency = start.elapsed().as_secs_f64();
+    let status = response.status().as_u16().to_string();
+
+    let labels = [
+        ("method", method.to_string()),
+        ("path", path),
+        ("status", status),
+    ];
+
+    metrics::counter!("http_requests_total", &labels).increment(1);
+    metrics::histogram!("http_requests_duration_seconds", &labels).record(latency);
+
+    response
+}
+
+fn main_app() -> Router {
+    Router::new()
         .route("/", get(handler))
+        .route("/test", get(handler))
         .fallback(handler_404)
+        .route_layer(middleware::from_fn(track_metrics))
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(|request: &Request<_>| {
@@ -64,7 +150,11 @@ async fn main() {
             TraceLayer::new_for_http(),
             TimeoutLayer::new(Duration::from_secs(10)),
         ))
-        .layer(ServiceBuilder::new().layer(CompressionLayer::new()));
+        .layer(ServiceBuilder::new().layer(CompressionLayer::new()))
+}
+
+async fn start_main_server() {
+    let app = main_app();
 
     let mut listenfd = ListenFd::from_env();
     let listener = match listenfd.take_tcp_listener(0).unwrap() {
@@ -75,37 +165,31 @@ async fn main() {
         None => TcpListener::bind("127.0.0.1:3000").await.unwrap(),
     };
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).with_graceful_shutdown(shutdown_signal()).await.unwrap();
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+fn metrics_app() -> Router {
+    let recorder_handle = setup_metrics_recorder();
+    Router::new().route("/metrics", get(move || ready(recorder_handle.render())))
 }
 
-async fn handler_404() -> Html<&'static str> {
-    Html("<h1>Not Found</h1>")
-}
+async fn start_metrics_server() {
+    let app = metrics_app();
 
-async fn shutdown_signal() {
-    let ctrl_c = async {
-        signal::ctrl_c()
-            .await
-            .expect("failed to install Ctrl+C handler");
+    let mut listenfd = ListenFd::from_env();
+    let listener = match listenfd.take_tcp_listener(0).unwrap() {
+        Some(listener) => {
+            listener.set_nonblocking(true).unwrap();
+            TcpListener::from_std(listener).unwrap()
+        }
+        None => TcpListener::bind("127.0.0.1:3001").await.unwrap(),
     };
-
-    #[cfg(unix)]
-    let terminate = async {
-        signal::unix::signal(signal::unix::SignalKind::terminate())
-            .expect("failed to install signal handler")
-            .recv()
-            .await;
-    };
-
-    #[cfg(not(unix))]
-    let terminate = std::future::pending::<()>();
-
-    tokio::select! {
-        _ = ctrl_c => {},
-        _ = terminate => {},
-    }
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
 }
